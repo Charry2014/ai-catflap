69a2d266eb8782053bccbb034761cbf51cced0cb
Charry2014 <58067238+Charry2014@users.noreply.github.com>
2024-02-07 22:28:28 +0100

    Major rework of state machine & evaluation classes

--- src/catflap/evaluation.py
+++ src/catflap/evaluation.py
@@ -2,25 +2,25 @@ from collections import deque
 import numpy as np
 from enum import Enum
 from base_logger import logger
-from base_logger import logger
 
-class CatDetection(int, Enum):
-    CAT_ALONE = 0,
-    CAT_WITH_MOUSE = 1,
-    UNDECIDED = 2
 
 class Stats:
-    '''Container class for a queue with a rolling average'''
+    '''Container class for a queue with a rolling average
+        Don't create these yourself, use the StatsFactory'''
     def __init__(self, name:str, window=5, initial=0.01):
         '''Create an array, with low initial values'''
         self._name = name
-        self._queue = deque([initial] * window, maxlen=window)
+        self._queue = deque(maxlen=window)
         self._prev_ma = 0
         self._delta = 0
         self._moving_average = 0
         self._count = 0
         self._min = initial
         self._max = initial
+        self._threshold = 1
+
+        self._status = None
+        self._triggered_result = None
 
     def __str__(self):
         return f"{self.__class__.__name__} {self._name} ma:{self._moving_average:.2f} min:{self._min:.2f} max:{self._max:.2f} cnt:{self._count} dta:{self._delta:.2f}"
@@ -41,6 +41,10 @@ class Stats:
     def count(self) -> int:
         return self._count
 
+    @property
+    def status(self):
+        return self._status
+
     def push(self, value):
         '''Add a new value to the queue and update the average, min, max, count'''
         self._queue.append(value)
@@ -53,79 +57,106 @@ class Stats:
             self._max = value
         if value < self._min:
             self._min = value
-        logger.info(f"{__class__.__name__} {self._name} pushing value: {value:.2f} == {self}")
+        # evaluate the result....
+        if self._moving_average >= self.average_threshold and self._count >= self.min_result_count:
+            self._status = self._triggered_result
+
+        logger.debug(f"{__class__.__name__} {self._name} pushed value: {value:.2f} == {self} result {self._status}")
+        return self._status
 
 
-# 
+class StatsFactory():
+    '''Helper class to create Stats objects correctly. The result enum MUST be ordered
+        the same way as the labels, and have an UNDECIDED element last'''
+    def __init__(self, config:str, result:Enum) -> None:
+        self._index = 0
+        self._config = config
+        self._result_enum = result
+        assert('thresholds' in config)
+        self._thresholds = dict([ (l['label'], l['values']) for l in config['thresholds'] ])
+
+    def create(self, label:str) -> Stats:
+        '''Creates a Stats object, correctly configured. Only labels that
+            have a configuration object are used - the rest are ignored.
+            All the attributes set in the evaluation json are turned into
+            attributes in the Stats object, as well as the result to return
+            when triggered.'''
+        stat = None
+        if label in self._thresholds:
+            stat = Stats(label)
+            [setattr(stat, l, self._thresholds[label][l]) for l in self._thresholds[label] ]
+            stat._triggered_result = self._result_enum(self._index)
+            stat._status = self._result_enum(len(self._result_enum)-1)
+
+        self._index += 1
+        return stat
+
+
 class Evaluation():
     '''Container class for evaluation statistics
-    The labels used are:
+    The labels used are defined in JSON, example:
     {"labels": ["Cat-alone", "Cat-with-mouse", "Background", "Cat-body"]}
-    Only Cat-alone and Cat-with-mouse are relevant for this evaluation
-    This evaluation will swing as more detections are evaluated, it makes
-    a recommendation if the flap should be locked or not, but the state 
-    machine makes the final determination.
+    This is the same json used by the TF Lite model
+    Evaluation details can be set with the config json - configure the relevant
+    labels and threshold levels. See examples provided.
+    The result_enum values must correspond to the labels used, and end in UNDECIDED,
+    ie. the default value when no determination has been made. It must be an int.
     '''
-    def __init__(self) -> None:
-        self._stats = {"Cat-alone": Stats("Cat-alone"), "Cat-with-mouse": Stats("Cat-with-mouse")}
-        self._flap_locked = False
+    def __init__(self, labels:list, config, result_enum: Enum, min=3, delta=0.30) -> None:
+        '''_stats is a dictionary { label, Eval() } to evaluate each
+        incoming result. 
+        The label Undecided is an extra value.
+        '''
+        labels.append('Undecided')
+        factory = StatsFactory(config, result_enum)
+        self._stats = dict([(l, factory.create(l)) for l in labels])
         self._count = 0
-        self._result = CatDetection.UNDECIDED
+        self._result = result_enum(len(result_enum)-1)
 
     def __str__(self) -> str:
         return f"{self._count}:{self._result}"
 
-    def _evaluate(self):
-        '''Run the evaluation on the recorded statistics - very simple here
-        Generally we err on the side of caution - any hint of a mouse is taken
-        as a positive, but the Cat-alone detection requires several strong
-        results before we assume it is true.
-
-        TODO - perhaps using size or shape of the bounding rect will help
-        choose detections with higher confidence. A good detection will most
-        likely be mostly square, or perhaps taller. A small detection rectangle
-        indicates the cat is far away; one that is too wide shows a false result
-        As an example - this image test_data/Cat-alone-63-20230805-042718.482_catcam.jpg
-        clearly has a mouse, but was falsely classified as Alone because the cat is 
-        too far away. In this case about 2m - the bounding rect area would have given a 
-        good weighting. In the whole sequence of images the logic below would have 
-        kept the flap locked, but the Cat-alone statistics are not enough
-        0:Cat-alone    0.62   BoundingRect(origin_x=310, origin_y=161, width=60, height=48)
-        '''
-        retval = CatDetection.UNDECIDED
-        if self._stats["Cat-with-mouse"].count < 4 and self._stats["Cat-alone"].count < 3:
-            # wait for a minimum number of results
-            pass
-        elif self._stats["Cat-with-mouse"].max > 0.45:
-            retval = CatDetection.CAT_WITH_MOUSE
-        elif self._stats["Cat-alone"].moving_average > 0.5:
-            retval = CatDetection.CAT_ALONE
-
-        logger.info(f"{self.__class__.__name__} evaluation result {retval.name}")
-        self._result = retval
-
-    def add_record(self, label:str, value:float) -> None:
+    def add_record(self, label:str, value:float) -> int:
         '''Add the record to the statistics'''
-        if label not in self._stats:
-            logger.error(f"Label {label} not used for this evaluation")
-            return
-        self._count += 1
-        self._stats[label].push(value)
-        self._evaluate()
+        retval = None
+        if self._stats[label] == None:
+            # this is a label that is not used in this evaluation
+            pass
+        else:
+            
+            retval = self._stats[label].push(value)
+        self._result = retval
+        return retval
 
     @property
-    def result(self) -> CatDetection:
-        '''Determines if the evaluation determines that the flap should be
-        locked or unlocked. This is just a recommendation. The state machine
-        will determine the action to perform.
-        '''
+    def result(self) -> int:
+        '''The result of the determination'''
         return self._result
 
+
+class CatDetection(int, Enum):
+    CAT_ALONE = 0
+    CAT_WITH_MOUSE = 1
+    BACKGROUND = 2
+    CAT_BODY = 3
+    UNDECIDED = 4
+
+
 def main():
-    e = Evaluation()
-    e.stats["Cat-alone"].push(0.55)
-    e.stats["Cat-alone"].push(0.60)
-    e.stats["Cat-alone"].push(0.65)
+    import json
+    with open ("./labels.json") as j:
+        labels = json.load(j)['labels']
+        with open ("./eval_config.json") as e:
+            config = json.load(e)
+
+    e = Evaluation(labels, config, CatDetection)
+    result = e.add_record("Background", 0.60)
+    result = e.add_record("Background", 0.60)
+    result = e.add_record("Cat-alone", 0.55)
+    result = e.add_record("Background", 0.60)
+    result = e.add_record("Cat-alone", 0.60)
+    result = e.add_record("Background", 0.60)
+    result = e.add_record("Cat-alone", 0.65)
     pass
 
 if __name__ == "__main__":
--- src/catflap/states.py
+++ src/catflap/states.py
@@ -1,8 +1,8 @@
 
 from statemachine import StateMachine, State
 from enum import Enum
+import sys
 from base_logger import logger
-
 from detections import TFLDetections, TFLDetection, BoundingRect
 from evaluation import Evaluation, CatDetection
 from catflapcontrol import CatFlapControl
@@ -10,16 +10,39 @@ from statetimer import StateTimer
 
 class States(int, Enum):
     IDLE = 1
-    UNLOCKED = 2
-    MOVEMENT_LOCKED = 3
-    MOUSE_LOCKED = 4
+    TRIGGERING = 2
+    UNLOCKED = 3
+    MOVEMENT_LOCKED = 4
+    MOUSE_LOCKED = 5
 
 
 from abc import ABC, abstractmethod
 
+
+class Event():
+    '''Basic event wrapper class so state machine stays more generic'''
+    def __init__(self, d):
+        self._label = d.label
+        pass
+
+    def __str__(self) -> str:
+        return self._label
+
+    @property
+    def detail(self):
+        return self._label
+    pass
+
+class GlobalData():
+    def __init__(self) -> None:
+        self.cat_flap_control = CatFlapControl()
+        self.evaluation = None
+        self.timeout_timer = None
+
+
 class TState(State, ABC):
     @abstractmethod
-    def run(self, event:TFLDetection) -> States:
+    def run(self, event:Event) -> States:
         pass
 
    
@@ -34,149 +57,200 @@ class TState(State, ABC):
 
 
 class Idle(TState):
+    # name = "idleState"
+
+    '''Idle state is slow monitoring of the camera, looking for movement'''
     def __init__(self, *args, **kwargs) -> None:
         super(Idle, self).__init__(*args, **kwargs)
-        self._id = States.IDLE
 
-    def run(self, event:TFLDetection, evaluation:Evaluation):
-        '''Idle always goes straight to movement locked'''
+    def run(self, event:Event, data:GlobalData) -> States:
+        '''
+        Detect movement
+        No movement? Sleep 1 second. Return States.IDLE
         evaluation.add_record(event.label, event.score)
-        return States.MOVEMENT_LOCKED
+        '''
+        # sys.sleep(1)
+        # return States.IDLE
+        return States.TRIGGERING
 
-    def on_enter_state(self, event:TFLDetection, state:TState, control:CatFlapControl) -> None:
+    def on_enter_state(self, event:Event, data:GlobalData) -> None:
         '''When we return to idle we unlock the cat flap, so cats can exit from the inside, and
         reset the evaluation class ready for the next event sequence'''
-        logger.info(f"Entering {self.__class__.__name__} state")
-        control.cat_flap_unlock()
-        logger.info(f"PUML idleState --> flapControl: cat-flap-unlock")
+        # data.cat_flap_control.cat_flap_unlock()
+        # logger.info(f"PUML idleState --> flapControl: cat-flap-unlock")
+        # data.evaluation = None
+        pass
         
-    def on_exit_state(self, event:TFLDetection, state:TState, control:CatFlapControl) -> None:
+    def on_exit_state(self, event:Event, data:GlobalData) -> None:
         '''Leaving idle state - create a newly initialised statistics class'''
         logger.debug(f"Exiting {self.__class__.__name__} state")
+
+
+class Triggering(TState):
+    # name = "triggeringState"
+
+    '''Movement was detected, now we are looking for a cat'''
+    def __init__(self, *args, **kwargs) -> None:
+        super(Triggering, self).__init__(*args, **kwargs)
+
+    def run(self, event:Event, data:GlobalData) -> States:
+        '''
+        Detect movement
+        No movement? Sleep 1 second. Return States.IDLE
+        evaluation.add_record(event.label, event.score)
+        '''
+        return States.MOVEMENT_LOCKED
+
+    def on_enter_state(self, event:Event, data:GlobalData) -> None:
+        '''When we return to idle we unlock the cat flap, so cats can exit from the inside, and
+        reset the evaluation class ready for the next event sequence'''
+        # logger.info(f"Entering {self.__class__.__name__} state")
+        # data.cat_flap_control.cat_flap_unlock()
+        # logger.info(f"PUML idleState --> flapControl: cat-flap-unlock")
+        pass
+
+    def on_exit_state(self, event:Event, data:GlobalData) -> None:
+        '''Leaving idle state - create a newly initialised statistics class'''
+        # logger.debug(f"Exiting {self.__class__.__name__} state")
+        '''Start the timeout timer on the transition out of idleState only'''
+        # self.timeout_timer = StateTimer(self._timeout_handle)
+        # self.timeout_timer.timer_start()
+        pass
 
 
 class MovementLocked(TState):
     def __init__(self, *args, **kwargs) -> None:
         super(MovementLocked, self).__init__(*args, **kwargs)
 
-    def run(self, event:TFLDetection, evaluation:Evaluation):
+    def run(self, event:Event, data:GlobalData) -> States:
         '''From Movement Locked we can either unlock (no mouse is there) or go into
         permanent lock when a mouse is detected.'''
         retval = States.MOVEMENT_LOCKED
-        evaluation.add_record(event.label, event.score)
-        if evaluation.result == CatDetection.CAT_WITH_MOUSE:
-            retval = States.MOUSE_LOCKED
-        elif evaluation.result == CatDetection.CAT_ALONE:
-            retval = States.UNLOCKED
+        # evaluation.add_record(event.label, event.score)
+        # if evaluation.result == CatDetection.CAT_WITH_MOUSE:
+        #     retval = States.MOUSE_LOCKED
+        # elif evaluation.result == CatDetection.CAT_ALONE:
+        #     retval = States.UNLOCKED
         return retval
 
-    def on_enter_state(self, event:TFLDetection, state:TState, control:CatFlapControl) -> None:
-        logger.info(f"Entering {self.__class__.__name__} state")
-        control.cat_flap_lock()
-        logger.info(f"PUML movementLockedState --> flapControl: cat-flap-lock")
+    def on_enter_state(self, event:Event, data:GlobalData) -> None:
+        #logger.info(f"Entering {self.__class__.__name__} state")
+        # control.cat_flap_lock()
+        logger.info(f"Doing something important here")
+        pass
+
 
 class Unlocked(TState):
     def __init__(self, *args, **kwargs) -> None:
         super(Unlocked, self).__init__(*args, **kwargs)
 
-    def run(self, event:TFLDetection, evaluation:Evaluation):
+    def run(self, event:Event, data:GlobalData) -> States:
         '''The state machine will remain unlocked until a new movement is
         detected or a timeout takes it back to idle'''
         retval = States.UNLOCKED
-        evaluation.add_record(event.label, event.score)
-        if evaluation.result == CatDetection.CAT_WITH_MOUSE:
-            retval = States.MOUSE_LOCKED
+        # evaluation.add_record(event.label, event.score)
+        # if evaluation.result == CatDetection.CAT_WITH_MOUSE:
+        #     retval = States.MOUSE_LOCKED
         return retval
 
 
-    def on_enter_state(self, event:TFLDetection, state:TState, control:CatFlapControl) -> None:
-        logger.info(f"Entering {self.__class__.__name__} state")
-        control.cat_flap_unlock()
-        logger.info(f"PUML unlockedState --> flapControl: cat-flap-unlock")
+    def on_enter_state(self, event:Event, data:GlobalData) -> None:
+        #logger.info(f"Entering {self.__class__.__name__} state")
+        # control.cat_flap_unlock()
+        #logger.info(f"PUML unlockedState --> flapControl: cat-flap-unlock")
+        pass
 
 class MouseLocked(TState):
     def __init__(self, *args, **kwargs) -> None:
         super(MouseLocked, self).__init__(*args, **kwargs)
 
-    def run(self, event:TFLDetection, evaluation:Evaluation):
+    def run(self, event:Event, data:GlobalData) -> States:
         retval = States.MOUSE_LOCKED
-        evaluation.add_record(event.label, event.score)
-        if evaluation.result == CatDetection.CAT_ALONE:
-            retval = States.MOVEMENT_LOCKED
+        # evaluation.add_record(event.label, event.score)
+        # if evaluation.result == CatDetection.CAT_ALONE:
+        #     retval = States.MOVEMENT_LOCKED
         return retval
 
-    def on_enter_state(self, event:TFLDetection, state:TState, control:CatFlapControl) -> None:
-        logger.info(f"Entering {self.__class__.__name__} state")
-        control.cat_flap_lock()
-        logger.info(f"PUML mouseLockedState --> flapControl: cat-flap-lock")
+    def on_enter_state(self, event:Event, data:GlobalData) -> None:
+        # logger.info(f"Entering {self.__class__.__name__} state")
+        # control.cat_flap_lock()
+        # logger.info(f"PUML mouseLockedState --> flapControl: cat-flap-lock")
+        pass
 
-    def on_exit_state(self, event:TFLDetection, state:TState, control:CatFlapControl) -> None:
-        logger.info(f"Exiting {self.__class__.__name__} state")
+    def on_exit_state(self, event:Event, data:GlobalData) -> None:
+        # logger.info(f"Exiting {self.__class__.__name__} state")
         pass
 
 class CatFlapFSM(StateMachine):
 
+    '''Define the states'''
     idleState = Idle(name=States.IDLE, initial=True)
+    triggeringState = Triggering(name=States.TRIGGERING)
     unlockedState = Unlocked(name=States.UNLOCKED)
     movementLockedState = MovementLocked(name=States.MOVEMENT_LOCKED)
     mouseLockedState = MouseLocked(name=States.MOUSE_LOCKED)
-
-    movementLock = idleState.to(movementLockedState) | mouseLockedState.to(movementLockedState)
-    unlock = movementLockedState.to(unlockedState)
-    returnIdle = movementLockedState.to(idleState) | mouseLockedState.to(idleState) |  unlockedState.to(idleState)
-    mouseLock = movementLockedState.to(mouseLockedState) | unlockedState.to(mouseLockedState)
+    '''State transitions. All same-state transitions are internal, which means they
+        don't trigger the _exit and _enter actions if the from and too states are 
+        the same.'''
+    triggering = idleState.to(triggeringState) | triggeringState.to.itself(internal=True)
+    movementLock = triggeringState.to(movementLockedState) | movementLockedState.to.itself(internal=True)
+    unlock = movementLockedState.to(unlockedState) | unlockedState.to.itself(internal=True)
+    returnIdle = movementLockedState.to(idleState) | mouseLockedState.to(idleState) |  unlockedState.to(idleState) | idleState.to.itself(internal=True)
+    mouseLock = movementLockedState.to(mouseLockedState) | unlockedState.to(mouseLockedState) | mouseLockedState.to.itself(internal=True)
 
 
     def __init__(self) -> None:
         # Must come before parent init as this calls the Idle.on_enter_state method
-        self.catflapcontrol = CatFlapControl()
+        self._data = GlobalData()
         StateMachine.__init__(self, CatFlapFSM.idleState)
         
         logger.info("PUML nullState -> idleState: Startup")
 
+        '''Lookups from States ID into the state variables, and the transitions'''
         self.states = {
             States.IDLE: self.idleState,
+            States.TRIGGERING : self.triggeringState,
             States.UNLOCKED: self.unlockedState,
             States.MOVEMENT_LOCKED: self.movementLockedState,
             States.MOUSE_LOCKED: self.mouseLockedState
         }
         self.transitions = {
             States.IDLE : self.returnIdle,
+            States.TRIGGERING: self.triggering,
             States.UNLOCKED : self.unlock,
             States.MOVEMENT_LOCKED : self.movementLock,
             States.MOUSE_LOCKED : self.mouseLock
             }
-        self.timeout_timer = None
-        self.evaluation = None
 
 
-    def event_handle(self, event:TFLDetection):
-        '''Handle detection events from the cat AI system'''
-        logger.info(f"Handling incoming detection event {event.detail} in state {self.current_state.id}")
-        if self.evaluation == None:
-            logger.debug(f"Reset the evaluation statistics to default values")
-            self.evaluation = Evaluation()
-        new_state = self.states[self.current_state.run(event, self.evaluation)]
-        logger.info(f"PUML {self.current_state.id} -> {new_state.id}: {event}")
+    def event_handle(self, event:Event):
+        '''Handle detection events from the cat AI system
+            This is the first step in handling a new event from outside'''
+        logger.debug(f"Handling incoming detection event {event.detail} in state {self.current_state.id}")
+        # if self.evaluation == None:
+        #     logger.debug(f"Reset the evaluation statistics to default values")
+        #     self.evaluation = Evaluation()
 
-        if self.current_state == self.idleState and new_state != self.idleState:
-            '''Start the timeout timer on the transition out of idleState only'''
-            self.timeout_timer = StateTimer(self._timeout_handle)
-            self.timeout_timer.timer_start()
-        elif self.current_state != self.idleState and new_state == self.idleState:
-            self.evaluation = None
-            self.timeout_timer.timer_stop()
+        # Run the event handler for the current state with the incoming event and
+        # the global data
+        self._data.new_state = self.states[self.current_state.run(event, self._data)]
 
-        if new_state != self.current_state:
-            action = self.transitions[new_state.name]
-            action(self)
-            self.current_state = new_state
+        # if self.current_state == self.idleState and new_state != self.idleState:
+        #     '''Start the timeout timer on the transition out of idleState only'''
+        #     # self.timeout_timer = StateTimer(self._timeout_handle)
+        #     # self.timeout_timer.timer_start()
+        # elif self.current_state != self.idleState and new_state == self.idleState:
+        #     self.evaluation = None
+        #     # self.timeout_timer.timer_stop()
+
+        # Call the state transition action
+        action = self.transitions[self._data.new_state.name]
+        action(self)
 
 
     def _timeout_handle(self, args):
         '''This is a watchdog type timer to ensure the cat flap is always opened
         when a movement sequence ends.'''
-        logger.info(f"Handling timeout event")
         logger.info(f"PUML {self.current_state.id} -> idleState: Timeout")
         self.evaluation = None
         self.returnIdle()
@@ -184,32 +258,58 @@ class CatFlapFSM(StateMachine):
     def exit(self):
         '''Called when the state machine must exit. Unlock cat flap and exit.'''
         logger.info(f"Handling state machine exit event")
-        self.timeout_timer.timer_cancel()
+        # self._data.timeout_timer.timer_cancel()
 
+    ''' ------------------ Generic Transition Events ------------------
+        These are sorted into order, called on specific transitions only
+    '''
     def before_transition(self, event, state):
-        pass
-        # logger.debug(f"Before '{event}', on the '{state.id}' state.")
-
-    def on_transition(self, event, state):
-        pass
-        # logger.debug(f"On '{event}', on the '{state.id}' state.")
+        '''1. The first step in a state transition
+            The state parameter is the current state'''
+        # logger.debug(f"{self.__class__.__name__} before_transition: event  '{event}', in state '{state.id}'.")
 
     def on_exit_state(self, event, state):
+        '''2. Exit the current state
+            The state parameter is the current state'''
+        logger.debug(f"{self.__class__.__name__} on_exit_state: event  '{event}', exiting state '{state.id}'.")
         if hasattr(state, "on_exit_state") == True:
-            logger.debug(f"Exiting '{state.id}' state from '{event}' event.")
-            state.on_exit_state(event, state, self.catflapcontrol)
+            state.on_exit_state(event, self._data)
+
+    def on_transition(self, event, state):
+        '''3. Ready to transition from the old state to the new one. 
+            The state parameter here is the state we are leaving'''
+        logger.info(f"PUML {self.current_state.id} -> {self._data.new_state.id}: {event}")
+        pass
 
     def on_enter_state(self, event, state):
+        '''4. Entering the new state - the state parameter here is now the new state
+            This will also enter idleState from __initial__'''
+        logger.debug(f"{self.__class__.__name__} on_enter_state: event '{event}', entering state '{state.id}'.")
         if hasattr(state, "on_enter_state") == True:
-            logger.debug(f"Entering '{state.id}' state from '{event}' event.")
-            state.on_enter_state(event, state, self.catflapcontrol)
+            state.on_enter_state(event, self._data)
 
     def after_transition(self, event, state):
         pass
         # logger.debug(f"After '{event}', on the '{state.id}' state.")
 
 
+    ''' ------------------ Specific Transition Events ------------------
+        These are called only for actual transitions and not for
+        'to-self' events. For the exit events state will be the current
+        state, for the enter events state is the new state.
+    '''
+    # def on_exit_idleState(self, event, state):
+    #     state.on_exit_state(event, state, self.catflapcontrol)
+    #     pass
+
+    # def on_enter_idleState(self, event, state):
+
+
+    # def on_enter_movementLockedState(self, event, state):
+    #     pass
+
 def main(data):
+
     # Create a test data structure - same as it would come from the ML system
     detections = TFLDetections("Name")
     for d in data:
@@ -219,7 +319,7 @@ def main(data):
     # Run the data set through the event handler
     fsm = CatFlapFSM()
     for d in detections:
-        fsm.event_handle(d)
+        fsm.event_handle(Event(d))
 
     import time
     i = 0
@@ -237,15 +337,21 @@ def main(data):
 # 1:Cat-with-mouse    0.55   BoundingRect(origin_x=67, origin_y=-5, width=441, height=181)
 # 1:Cat-with-mouse    0.36   BoundingRect(origin_x=177, origin_y=-2, width=454, height=255)
 # 1:Cat-with-mouse    0.36   BoundingRect(origin_x=58, origin_y=13, width=563, height=382)
-
-#    BoundingBox(origin_x=190, origin_y=327, width=109, height=113
+# 
+# BoundingBox(origin_x=190, origin_y=327, width=109, height=113
 data = [
-            (0, "Cat-alone",    0.53, 190, 327,109,113),
-            (0, "Cat-alone",    0.55, 194, 337,119,133),
-            (0, "Cat-alone",    0.59, 195, 347,122,183),
-            (0, "Cat-alone",    0.63, 199, 357,199,193)
+        (2, "Background", 0.53, 190, 327,109,113),
+        (2, "Background", 0.53, 190, 327,108,113),
+        (2, "Background", 0.53, 190, 327,107,113),
+        (2, "Background", 0.53, 190, 327,106,113),
+        (0, "Cat-alone",  0.53, 190, 327,105,113),
+        (0, "Cat-alone",  0.53, 190, 327,104,113),
+        (0, "Cat-alone",  0.53, 190, 327,103,113),
+        (0, "Cat-alone",  0.55, 194, 337,119,133),
+        (0, "Cat-alone",  0.59, 195, 347,122,183),
+        (0, "Cat-alone",  0.63, 199, 357,199,193)
        ]
 
 if __name__ == "__main__":
-    logger.basicConfig(level=logger.DEBUG)
+    # logger.basicConfig(level=logger.DEBUG)
     main(data)
