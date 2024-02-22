ca1ada5e86b305421dbcb1dd69327dfb3b35c275
Charry2014 <58067238+Charry2014@users.noreply.github.com>
2024-02-22 22:15:39 +0100

    Create dir, and roll over file name each restart

--- src/modules/base_logger.py
+++ src/modules/base_logger.py
@@ -1,6 +1,9 @@
 '''A hacky logging wrapper to log both to the console and a file
 '''
 import logging
+import os
+from datetime import datetime
+
 
 _format_string = '%(asctime)s %(module)s(%(lineno)d):%(levelname)s %(message)s'
 logger = logging.NullHandler()
@@ -9,9 +12,25 @@ logging.basicConfig(format=_format_string,
         level=logging.DEBUG,
         datefmt='%Y%m%d %H:%M:%S')
 logger = logging.getLogger(__name__)
-filelogger = logging.FileHandler(filename='./log/catflap.log')
+
+directory_path = './log/'
+file_name = 'catflap.log'
+
+if not os.path.exists(directory_path):
+        try:
+                os.makedirs(directory_path)
+                print(f"Directory '{directory_path}' created successfully.")
+        except OSError as e:
+                print(f"Error creating directory '{directory_path}': {e}")
+elif os.path.exists(os.path.join(directory_path, file_name)):
+       current_datetime = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+       base, ext = os.path.splitext(file_name)
+       new_filename = f"{base}_{current_datetime}{ext}"
+       os.rename(os.path.join(directory_path, file_name), os.path.join(directory_path, new_filename))
+
+filelogger = logging.FileHandler(filename=directory_path + file_name)
 filelogger.setFormatter(logging.Formatter(_format_string))
-filelogger.setLevel(logging.DEBUG)
+filelogger.setLevel(logging.INFO)
 
 logger.addHandler(filelogger)
 
